043fe6310979e9789cf0f7d58e61c917e6e3ce05
Jared Lund <jared.lund@icloud.com>
2017-03-21 13:58:19 -0600

    Created a comment broadcast, configured web socket route, and updated the comment model with a callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :blog
 
   validates :content, presence: true, length: {minimum: 10, maximum: 1000 }
-
+  after_create_commit { CommentBradcastjob.perform_later(self) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
-    put :sort, on: :collection
+    put :sort, on: :collection              
   end
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
@@ -16,6 +15,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+  
+  mount AcgtionCable.server => '/cable'
   
   root to: 'pages#home'
 end
